894b95dd9b062494bc03d9984f302c91282d0de9
Talya Eliyahu <talyaeli@post.bgu.ac.il>
2025-01-28 13:13:16 +0200

    persistence start

--- persistence.py
+++ persistence.py
@@ -4,31 +4,60 @@ from dbtools import Dao
  
 # Data Transfer Objects:
 class Employee(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, name, salary, branche) :
+        self.id = id
+        self.name = name
+        self.salary = salary
+        self.branch = branche
+
+    def __str__(self) :
+        return f"({self.id}, '{self.name}', {self.salary}, {self.branch})"
+
  
 class Supplier(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, name, contact_information) :
+        self.id = id
+        self.name = name
+        self.contact_information = contact_information
+    def __str__(self):
+        return f"({self.id}, '{self.name}', '{self.contact_information}')"
 
 class Product(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, description, price, quantity) :
+        self.id = id
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+    def __str__(self):
+        return f"({self.id}, '{self.description}', {self.price}, {self.quantity})"
 
 class Branche(object):
-    #TODO: implement
-    pass
+    def __init__(self, id, location, number_of_employees) :
+        self.id = id
+        self.location = location
+        self.number_of_employees = number_of_employees
+    def __str__(self):
+        return f"({self.id}, '{self.location}', {self.number_of_employees})"
 
 class Activitie(object):
-    #TODO: implement
-    pass
+    def __init__(self, product_id, quantity, activator_id, date) :
+        self.product_id = product_id
+        self.quantity = quantity
+        self.activator_id = activator_id
+        self.date = date
+    def __str__(self):
+        return f"({self.product_id}, {self.quantity}, {self.activator_id}, '{self.date}')"
  
  
 #Repository
 class Repository(object):
     def __init__(self):
         self._conn = sqlite3.connect('bgumart.db')
-        #TODO: complete
+        self._employees = Dao(Employee, self._conn)
+        self._suppliers = Dao(Supplier, self._conn)
+        self._products = Dao(Product, self._conn)
+        self._branches = Dao(Branche, self._conn)
+        self._activities = Dao(Activitie, self._conn)
  
     def _close(self):
         self._conn.commit()
